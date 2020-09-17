be870153ea6de718c865246a25b8ca3f3d63a96e
calizarazo <calizarazo@indracompany.com>
2020-09-16 22:09:36 -0500

    complement comments

--- src/app/services/characters/characters.service.ts
+++ src/app/services/characters/characters.service.ts
@@ -5,13 +5,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class CharactersService {
+  //definición de URL base
   private apiUrl = 'https://rickandmortyapi.com/api/';
   private allRequest = `${this.apiUrl}character`;
 
   constructor(private http: HttpClient) {}
 
+  //servicio que retorna todos los personajes
   getCharactersPaginator = (page) =>
     this.http.get(`${this.allRequest}?page=${page}`);
 
+  //servicio generico para solicitudes GET
   GET = (url) => this.http.get(`${url}`);
 }
